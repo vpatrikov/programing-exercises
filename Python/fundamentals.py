f781c62616ebdb290651352f39b777b7d03b26d6
Vladimir Patrikov <vladimir.n.patrikov@gmail.com>
2024-05-24 11:47:18 +0300

    Update fundamentals.py

--- Python/fundamentals.py
+++ Python/fundamentals.py
@@ -1,2 +1,11 @@
 def multiply(n):
     return n * 5**len(str(abs(n)))
+
+def to_alternating_case(string):
+    result = []
+    for char in string:
+        if char.islower():
+            result.append(char.upper())
+        else:
+            result.append(char.lower())
+    return ''.join(result)
